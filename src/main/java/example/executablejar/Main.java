fc7eae2cb0d6572424ad2d2d31f1c121a88ce6d3
gregwhitaker <gwhitake@gmail.com>
2019-12-03 08:53:42 -0800

    Doing work

--- src/main/java/example/executablejar/Main.java
+++ src/main/java/example/executablejar/Main.java
@@ -1,8 +1,38 @@
 package example.executablejar;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import example.executablejar.model.ResponseModel;
+import okhttp3.OkHttpClient;
+import okhttp3.Request;
+import okhttp3.Response;
+
+import java.util.Properties;
+import java.util.Random;
+
 public class Main {
 
-    public static void main(String... args) {
-        System.out.println("Hello World!");
+    public static void main(String... args) throws Exception {
+        Properties props = new Properties();
+        props.load(Main.class.getClassLoader().getResourceAsStream("application.properties"));
+
+        System.out.println("Loaded property from property file: " + props.get("test.property"));
+
+        Random rand = new Random(System.currentTimeMillis());
+        OkHttpClient httpClient = new OkHttpClient();
+        ObjectMapper mapper = new ObjectMapper();
+
+        Request request = new Request.Builder()
+                .get()
+                .url("http://httpbin.org/get?test=" + rand.nextInt((100 - 1) + 1) + 1)
+                .build();
+
+        try (Response response = httpClient.newCall(request).execute()) {
+            if (response.isSuccessful()) {
+                ResponseModel model = mapper.readerFor(ResponseModel.class).readValue(response.body().bytes());
+                System.out.println("Response from HTTP endpoint: " + model.getArgs().get("test"));
+            } else {
+                System.out.println("Error contacting mock http service: " + response.code());
+            }
+        }
     }
 }
---
+++ src/main/java/example/executablejar/model/ResponseModel.java
@@ -0,0 +1,43 @@
+package example.executablejar.model;
+
+import java.util.Map;
+
+public class ResponseModel {
+
+    private Map<String, String> args;
+    private Map<String, String> headers;
+    private String origin;
+    private String url;
+
+    public Map<String, String> getArgs() {
+        return args;
+    }
+
+    public void setArgs(Map<String, String> args) {
+        this.args = args;
+    }
+
+    public Map<String, String> getHeaders() {
+        return headers;
+    }
+
+    public void setHeaders(Map<String, String> headers) {
+        this.headers = headers;
+    }
+
+    public String getOrigin() {
+        return origin;
+    }
+
+    public void setOrigin(String origin) {
+        this.origin = origin;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
